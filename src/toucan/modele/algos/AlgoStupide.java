9bff943eb8353600b66e21c6b4412e7eb6494d47
bertra162u <matt.bert@live.fr>
2018-10-08 13:08:10 +0200

    Bonus: Algo Stupide(2)

---
+++ src/toucan/modele/algos/AlgoStupide.java
@@ -0,0 +1,45 @@
+package toucan.modele.algos;
+
+import toucan.modele.LesCases;
+
+import java.util.Random;
+
+import static toucan.modele.animation.AttributAnimation.*;
+
+
+public class AlgoStupide  extends Algo{
+
+    public AlgoStupide(LesCases lesCases, int... entiers) {
+        super(lesCases, entiers);
+    }
+
+    @Override
+    public void trier() {
+        Random random = new Random();
+        int temp;
+        int rVal1;
+        int rVal2;
+        while (!estTrie()) {
+            rVal1 = random.nextInt(this.tabEntiers.length - 1);
+            do {
+                rVal2 = random.nextInt(this.tabEntiers.length - 1);
+            } while(rVal1 == rVal2);
+            temp = tabEntiers[rVal1];
+            tabEntiers[rVal1] = tabEntiers[rVal2];
+            tabEntiers[rVal2] = temp;
+            this.executerAux(AFFECTATION, rVal1, rVal2);
+        }
+    }
+
+    public boolean estTrie() {
+        boolean trier = true;
+        for (int i = 0; i < this.tabEntiers.length - 1 ; i++) {
+            this.executerAux(COMPARAISON, i, i+1);
+            System.out.println(this.tabEntiers[i] + " / " +this.tabEntiers[i+1]);
+            if (this.tabEntiers[i] > this.tabEntiers[i+1]) {
+                trier = false;
+            }
+        }
+        return trier;
+    }
+}
